15624219301099a11acd2617543e10a1d2b28663
AIC Digital <53875525+PWPankaj@users.noreply.github.com>
2021-05-23 20:22:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,248 @@
+
+
+<?php include 'header.php';?>
+
+
+
+<!--------------------------- SLIDER ------------------------>
+    <div class="slider">
+
+      <div class="slideshow-container">
+        <div class="mySlides fade">
+          <img src="images/slider_image.jpg">
+          <div class="text">Boost Your Career with us.</div>
+        </div>
+
+        <div class="mySlides fade">
+          <img src="images/slider_image.jpg">
+          <div class="text">Learn From the experts.</div>
+        </div>
+
+        <div class="mySlides fade">
+          <img src="images/slider_image.jpg">
+          <div class="text">Gain industry knowledge.</div>
+        </div>
+
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+      </div>
+      <br>
+
+      <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+      </div>
+    </div>
+
+    <!--------------------- Training ----------------------->
+    <div class="training">
+      <div class="training__card">
+        <div class="training__logo"><img src="icon/Carporate_training.png" alt="Corporate Training  Icon"></div>
+        <div class="training__header">CORPORATE TRAINING</div>
+        <div class="training__desc">We train corporates as per their requirements in order to work more effectively and
+          efficiently</div>
+      </div>
+      <div class="training__card">
+        <div class="training__logo"><img src="icon/classroom_training.png" alt="Clasroom Training Icon"></div>
+        <div class="training__header">CLASSROOM TRAINING</div>
+        <div class="training__desc">We also take class rooms trainings for students in order to work more effectively
+          and
+          efficiently</div>
+      </div>
+      <div class="training__card">
+        <div class="training__logo"><img src="icon/Whats_new.png" alt="What's New Icon"></div>
+        <div class="training__header">WHAT'S NEW</div>
+        <div class="training__desc">Get updates about our upcoming news and our latest offers.</div>
+      </div>
+    </div>
+    <!-- -------------------Our Key Points---------------- -->
+    <div class="keypoints">
+      <div class="keypoint__img"><img src="images/keypoints.jpg" alt=""></div>
+      <div class="keypoints__content">
+        <h3>OUR KEY POINTS</h3>
+        <p>We, <strong>"iVERTEX"</strong> have expertise in providing high end training programs in System
+          Administration,
+          Networking, Server virtualization, Cloud computing and Information Security.</p>
+        <ul>
+          <li>Highly qualified and certified trainers.</li>
+          <li>Equipped Labs with the latest systems.</li>
+          <li>Getting Experienced While Learning.</li>
+        </ul>
+
+      </div>
+
+    </div>
+    <!-- ------------------Our Courses ----------------------->
+    <div class="courses">
+      <h3>OUR COURSES</h3>
+      <div class="course__row">
+        <div class="course__card">
+          <div class="course__icon"><img src="icon/hardware_networking.png" alt="HARDWARE & NETWORKING"></div>
+          <div class="course__name">DEVOPS</div>
+        </div>
+        <div class="course__card">
+          <div class="course__icon"><img src="icon/system_admin.png" alt="SYSTEM ADMINISTRATION"></div>
+          <div class="course__name">ANSIBLE</div>
+        </div>
+        <div class="course__card">
+          <div class="course__icon"><img src="icon/networking.png" alt="NETWORKING"></div>
+          <div class="course__name">CISCO</div>
+        </div>
+      </div>
+      <div class="course__row">
+        <div class="course__card">
+          <div class="course__icon"><img src="icon/server_virtulization.png" alt="SERVER VIRTUALIZATION"></div>
+          <div class="course__name">RHCE</div>
+        </div>
+        <div class="course__card">
+          <div class="course__icon"><img src="icon/cloud_computing.png" alt="CLOUD COMPUTING"></div>
+          <div class="course__name">CLOUD COMPUTING</div>
+        </div>
+        <!-- <div class="course__card">
+          <div class="course__icon"><img src="./icon/info_security.png" alt="INFORMATION SECURITY"></div>
+          <div class="course__name">INFORMATION SECURITY</div>
+        </div> -->
+      </div>
+    </div>
+    <!-- ----------------------Our Student Say----------------- -->
+    <div class="review">
+      <h3>OUR STUDENTS SAY</h3>
+      <div class="slideshow-container1">
+
+        <div class="slideshow-container1">
+          <div class="mySlides1">
+            <div class="review__card">
+              <div class="review__card-icon"><img src="icon/student_icon.png" alt="studentIcon"></div>
+              <div class="review__card-say">
+                <p>This is a great institute, I got a good practical experience which helped me a lot in my day to day
+                  tasks. The institute really helps me to understand all the concepts thoroughly. The Faculty members
+                  are really nice and great.</p>
+                <div class="review__card-name">- Suprobhat Maity</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="mySlides1">
+            <div class="review__card">
+              <div class="review__card-icon"><img src="icon/student_icon.png" alt="studentIcon"></div>
+              <div class="review__card-say">
+                <p>I have completed course in Ivertex Technologies RHCSA and RHCE. Instructors will focus on each
+                  student and make sure people understand the topic. I have seen people attend various courses and
+                  everyone is going 100% satisfied and confident. special thanks for Disha Madam.
+                </p>
+                <div class="review__card-name">- Vijay Kathole</div>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="mySlides1">
+            <div class="review__card">
+              <div class="review__card-icon"><img src="icon/student_icon.png" alt="studentIcon"></div>
+              <div class="review__card-say">
+                <p>iVERTEX Technologies is a brilliant platform to start learning new and updated technologies..I did
+                  RHCSA and RHEL courses from here with global certification..staff is very good and supportive...while
+                  preparing for the exams, staff give attention to every student personally.</p>
+                <div class="review__card-name">- Snehal Hande</div>
+              </div>
+            </div>
+          </div>
+          <div class="mySlides1">
+            <div class="review__card">
+              <div class="review__card-icon"><img src="icon/student_icon.png" alt="studentIcon"></div>
+              <div class="review__card-say">
+                <p>For linux iVertex is the best option in pune.
+                  Flexible time provided for working student, extra timing allowed for practice. Fully supporting staff.
+                </p>
+                <div class="review__card-name">- Vishal Mhaskar</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="mySlides1">
+            <div class="review__card">
+              <div class="review__card-icon"><img src="icon/student_icon.png" alt="studentIcon"></div>
+              <div class="review__card-say">
+                <p>Ivertex Pune complete team is very commited for student knowledge ,
+                  continiously support to student to overcome their weakness.</p>
+                <div class="review__card-name">- Ajay Kulkarni</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="mySlides1">
+            <div class="review__card">
+              <div class="review__card-icon"><img src="icon/student_icon.png" alt="studentIcon"></div>
+              <div class="review__card-say">
+                <p>It's a amazing learning experience here. Infra is adequate. Instructor's are knowledgeable and
+                  professional. Diksha is my class instructor. She is modest, knowledgeable and make learning very easy.
+                  The syllabus covered is 100℅ match to the certification. You can put your time to practice the Labs
+                  and gain thorough knowledge. Sharing knowledge is the prime moto here. A must place for serious
+                  learners.
+                  Thanks iVertex. Wish you best wishes.</p>
+                <div class="review__card-name">- Prashant Pimprikar</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="mySlides1">
+            <div class="review__card">
+              <div class="review__card-icon"><img src="icon/student_icon.png" alt="studentIcon"></div>
+              <div class="review__card-say">
+                <p>I was completed CCNP (CISCO CERTIFIED NETWORK PROFESSIONAL) In iVERTEX Technologies & iVERTEX helped
+                  me for learn about CCNP , about Networking . Because Teaching Staff & Other staff is so Good in they
+                  works . Teaching staff haved excellent Knowledge About courses and counseling. In practical lectures
+                  Institutes give real equipments Like Routers , Hubs. Beginner students can practicing very well about
+                  CCNP lectures and Other related courses.</p>
+                <div class="review__card-name">- Sudarshan Shukla</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="mySlides1">
+            <div class="review__card">
+              <div class="review__card-icon"><img src="icon/student_icon.png" alt="studentIcon"></div>
+              <div class="review__card-say">
+                <p>Very professional and knowledgeable faculties. They make sure every student understand even most
+                  complex technological concepts. The realistic examples shared while coaching help understand
+                  technology easily. 5 stars for iVERTEX</p>
+                <div class="review__card-name">- Jayant Bhagwat</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="mySlides1">
+            <div class="review__card">
+              <div class="review__card-icon"><img src="icon/student_icon.png" alt="studentIcon"></div>
+              <div class="review__card-say">
+                <p>iVERTEX is an institute where your expectation should have no limit.
+                  iVERTEX is an institute where you can achive your Goals.
+                  iVERTEX (The sessions i had was like cristal clear) AMAZING.</p>
+                <div class="review__card-name">- Blesson George</div>
+              </div>
+            </div>
+          </div>
+
+
+
+          <a class="prev1" onclick="plusSlides1(-1)">❮</a>
+          <a class="next1" onclick="plusSlides1(1)">❯</a>
+
+        </div>
+      </div>
+    </div>
+    <!-- ------------------Learn More----------------------- -->
+    <div class="learn">
+      <div>
+        <h3>LEARN FROM EXPERTS</h3>
+      </div>
+      <div><button>LEARN MORE</button></div>
+    </div>
+    <!--------------------Footer----------------------------- -->
+    <?php include 'footer.php';?>
+
+</body>
+
+</html>
